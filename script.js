d56fa057cb1f2dd5777e6c0dbde1b1a5154c2337
Ashutosh <78640870+ashut814@users.noreply.github.com>
2023-11-13 11:58:16 +0530

    f4

--- script.js
+++ script.js
@@ -13,7 +13,17 @@ document.addEventListener('DOMContentLoaded', function () {
         const booking = { name, email, phone, timeForCall };
 
         const bookings = JSON.parse(localStorage.getItem('bookings')) || [];
-        bookings.push(booking);
+        const editIndex = form.dataset.editIndex;
+
+        if (editIndex !== undefined) {
+            // If editing, replace the existing entry
+            bookings[editIndex] = booking;
+            form.removeAttribute('data-edit-index'); // Reset the edit state
+        } else {
+            // If not editing, add a new entry
+            bookings.push(booking);
+        }
+
         localStorage.setItem('bookings', JSON.stringify(bookings));
 
         // Clear existing content and display bookings
@@ -35,6 +45,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     <p>Email: ${booking.email}</p>
                     <p>Phone: ${booking.phone}</p>
                     <p>Time for Call: ${booking.timeForCall}</p>
+                    <button class="edit-button" data-index="${index}">Edit</button>
                     <button class="delete-button" data-index="${index}">Delete</button>
                 </div>
             `;
@@ -43,11 +54,32 @@ document.addEventListener('DOMContentLoaded', function () {
         // Append new content
         displayArea.insertAdjacentHTML('beforeend', bookingsHtml);
 
-        // Add event listeners to delete buttons
+        // Add event listeners to edit and delete buttons
+        const editButtons = document.querySelectorAll('.edit-button');
         const deleteButtons = document.querySelectorAll('.delete-button');
+
+        editButtons.forEach(button => {
+            button.addEventListener('click', handleEdit);
+        });
+
         deleteButtons.forEach(button => {
             button.addEventListener('click', handleDelete);
         });
+    }
+
+    function handleEdit(event) {
+        const index = event.target.dataset.index;
+        const bookings = JSON.parse(localStorage.getItem('bookings')) || [];
+        const bookingToEdit = bookings[index];
+
+        // Populate the form with the existing data
+        document.getElementById('name').value = bookingToEdit.name;
+        document.getElementById('email').value = bookingToEdit.email;
+        document.getElementById('phone').value = bookingToEdit.phone;
+        document.getElementById('time-for-call').value = bookingToEdit.timeForCall;
+
+        // Set the edit state on the form
+        form.dataset.editIndex = index;
     }
 
     function handleDelete(event) {
